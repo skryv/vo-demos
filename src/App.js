a4796103f501c31467e4e8fb86b744eaf7284fb3
Arno Schutijzer <arno.schutijzer@skryv.com>
2020-12-04 15:51:23 +0100

    add initial setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import '@govflanders/vl-ui-core';
+import '@govflanders/vl-ui-util';
+
+function App() {
+  return (
+    <>
+      <h1>hello world</h1>
+      <p>hello world</p>
+    </>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+import App from './App';
+
+import '@govflanders/vl-ui-core/dist/font/iconfont/vlaanderen-icon.woff2';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
